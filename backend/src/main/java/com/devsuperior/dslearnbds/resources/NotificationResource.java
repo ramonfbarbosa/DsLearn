b886efd0afb9fd1ce8cc6d6dd273ef8f92350c70
Ramon Barbosa <ramonmfb777@gmail.com>
2021-12-03 16:31:27 -0300

    Unread/read notification implement

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -6,10 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
 import com.devsuperior.dslearnbds.services.NotificationService;
+
+import net.bytebuddy.implementation.bytecode.constant.DefaultValue;
 
 @RestController
 @RequestMapping(value = "/notifications")
@@ -19,8 +22,10 @@ public class NotificationResource {
 	private NotificationService service;
 
 	@GetMapping
-	public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(Pageable pageable){
-		Page<NotificationDTO> page = service.notificationForCurrentUser(pageable);
+	public ResponseEntity<Page<NotificationDTO>> notificationForCurrentUser(
+			@RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly, 
+			Pageable pageable){
+		Page<NotificationDTO> page = service.notificationForCurrentUser(unreadOnly, pageable);
 		return ResponseEntity.ok().body(page);
 	}
 }
